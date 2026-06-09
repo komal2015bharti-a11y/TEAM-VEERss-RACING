9577dc459894ff44d6cb7664f3016f07fe043ff9
komal2015bharti-a11y <komal2015bharti@gmail.com>
2026-06-09 15:04:08 +0530

    New site

---
+++ script.js
@@ -0,0 +1,60 @@
+
+/* REVEAL ANIMATION */
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", revealSections);
+
+function revealSections(){
+
+  reveals.forEach((element)=>{
+
+    const windowHeight = window.innerHeight;
+
+    const revealTop = element.getBoundingClientRect().top;
+
+    const revealPoint = 120;
+
+    if(revealTop < windowHeight - revealPoint){
+
+      element.classList.add("active");
+
+    }else{
+
+      element.classList.remove("active");
+
+    }
+
+  });
+
+}
+
+revealSections();
+
+/* IMAGE POPUP */
+
+const images = document.querySelectorAll(".gallery img");
+
+const popup = document.getElementById("popup");
+
+const popupImg = document.getElementById("popupImg");
+
+const closePopup = document.getElementById("closePopup");
+
+images.forEach((img)=>{
+
+  img.addEventListener("click", ()=>{
+
+    popup.style.display = "flex";
+
+    popupImg.src = img.src;
+
+  });
+
+});
+
+closePopup.addEventListener("click", ()=>{
+
+  popup.style.display = "none";
+
+});
