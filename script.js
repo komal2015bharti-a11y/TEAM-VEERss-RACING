f380d2910fa4bc8b0181c9ed0aa885e88af3a624
komal2015bharti-a11y <komal2015bharti@gmail.com>
2026-06-09 18:22:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,3 +70,44 @@ menuToggle.addEventListener("click", ()=>{
   navLinks.classList.toggle("active");
 
 });
+
+/* TEAM SLIDER */
+
+const slides = document.querySelectorAll(".member-slide");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+  slides.forEach((slide)=>{
+
+    slide.classList.remove("active");
+
+  });
+
+  slides[index].classList.add("active");
+}
+
+function nextSlide(){
+
+  currentSlide++;
+
+  if(currentSlide >= slides.length){
+
+    currentSlide = 0;
+  }
+
+  showSlide(currentSlide);
+}
+
+function prevSlide(){
+
+  currentSlide--;
+
+  if(currentSlide < 0){
+
+    currentSlide = slides.length - 1;
+  }
+
+  showSlide(currentSlide);
+}
